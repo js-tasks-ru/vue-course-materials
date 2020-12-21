b57c61f8c55e0932eab1381e3ce7ffd460163286
Grigorii K. Shartsev <me@shgk.me>
2020-12-21 21:26:59 +0500

    Start 03-vue-cli

---
+++ 03-vue-cli/03-Scoped-styles/src/AppText.vue
@@ -0,0 +1,14 @@
+<template>
+  <span class="root">
+    <span class="deep">{{ text }}</span>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'AppText',
+  props: ['text'],
+};
+</script>
+
+<style></style>
---
+++ 03-vue-cli/03-Scoped-styles/src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import App from './App.vue';
+import './styles.css';
+
+new Vue({
+  render: (h) => h(App),
+}).$mount('#app');
